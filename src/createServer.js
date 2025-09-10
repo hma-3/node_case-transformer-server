12055fce7a31dc91010c49684d7aa28a2b899c35
Mariia Hula <maria.gula0307@gmail.com>
2025-09-11 00:36:29 +0300

    fix according to comments

--- src/createServer.js
+++ src/createServer.js
@@ -4,18 +4,17 @@ const { convertToCase } = require('./convertToCase');
 
 const createServer = () =>
   http.createServer((req, res) => {
-    const { pathname, searchParams } = new URL(
-      req.url,
-      `http://${req.headers.host}`,
-    );
-    const originalText = pathname.slice(1);
-    const targetCase = searchParams.get('toCase');
+    const [path, queryString] = req.url.split('?');
+    const originalText = (path || '').slice(1);
+    const params = new URLSearchParams(queryString || '');
+    const targetCase = params.get('toCase');
     const errors = validate(originalText, targetCase);
 
-    res.setHeader('Content-type', 'application/json');
+    res.setHeader('Content-Type', 'application/json');
 
     if (errors.length) {
       res.statusCode = 400;
+      res.statusText = 'Bad request';
       res.end(JSON.stringify({ errors }));
 
       return;
@@ -27,6 +26,7 @@ const createServer = () =>
     );
 
     res.statusCode = 200;
+    res.statusText = 'OK';
 
     res.end(
       JSON.stringify({
